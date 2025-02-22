0dcc3039b3b93df8354a007c436de71b9a3f7461
Ayana Ussenbayeva <164485227+Idyxdoxiwowl@users.noreply.github.com>
2025-02-23 00:49:30 +0600

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { registerUser, loginUser, getProfile, updateProfile } = require("../controllers/authController");
-const { authenticate } = require("../middleware/authMiddleware");
+const { registerUser, loginUser, getProfile, updateProfile, getUsers, deleteUser } = require("../controllers/authController");
+const { authenticate, checkAdmin } = require("../middleware/authMiddleware");
+
 const router = express.Router();
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", authenticate, getProfile);
 router.put("/profile", authenticate, updateProfile);
+
+// Новый маршрут для получения всех пользователей (только для админа)
+router.get("/users", authenticate, checkAdmin, getUsers);
+
+// Новый маршрут для удаления пользователей (только для админа)
+router.delete("/users/:id", authenticate, checkAdmin, deleteUser);
+
 module.exports = router;
