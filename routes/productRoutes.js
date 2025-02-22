08b21a438f4da7f4163e1348dcacaef60dd2e1e6
Ayana Ussenbayeva <164485227+Idyxdoxiwowl@users.noreply.github.com>
2025-02-22 23:02:11 +0600

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,8 +5,8 @@ const { authenticate, checkAdmin } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/", getProducts); // Получение всех товаров
-router.post("/", authenticate, checkAdmin, createProduct); // Добавление товара (только админ)
-router.put("/:id", authenticate, checkAdmin, updateProduct); // Обновление товара (только админ)
-router.delete("/:id", authenticate, checkAdmin, deleteProduct); // Удаление товара (только админ)
+router.post("/", authenticate, checkAdmin, createProduct); // Только для админа
+router.put("/:id", authenticate, checkAdmin, updateProduct);
+router.delete("/:id", authenticate, checkAdmin, deleteProduct);
 
 module.exports = router;
