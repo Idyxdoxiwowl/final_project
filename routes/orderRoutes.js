c457493d6dc19838c6a4e970fe4fafa9e001ddbf
Ayana Ussenbayeva <164485227+Idyxdoxiwowl@users.noreply.github.com>
2025-02-25 20:31:21 +0600

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createOrder, getOrderById, deleteOrder } = require("../controllers/orderController");
+const { createOrder, getOrders, getOrderById, deleteOrder } = require("../controllers/orderController");
 const { authenticate } = require("../middleware/authMiddleware");
 const router = express.Router();
 router.post("/", authenticate, createOrder);
+router.get("/", authenticate, getOrders);
 router.get("/:id", authenticate, getOrderById);
 router.delete("/:id", authenticate, deleteOrder);
 module.exports = router;
