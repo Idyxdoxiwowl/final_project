84f54941b7c1a6945a50654548a83ebd83ba1ffa
Ayana Ussenbayeva <164485227+Idyxdoxiwowl@users.noreply.github.com>
2025-02-25 20:14:53 +0600

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -2,15 +2,14 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
     ref: { type: String, required: true }, // Уникальный код товара
-    category: { type: String, required: true }, // Категория (кольца, браслеты и т.д.)
-    name: { type: String, required: true }, // Название
-    price: { type: Number, required: true }, // Цена
-    tags: { type: [String], default: [] }, // Теги (платина, золото и т.д.)
-    description: { type: String, required: true }, // Описание
-    image: { type: String, required: true } // Ссылка на изображение
+    category: { type: String, required: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    tags: { type: [String], default: [] }, 
+    description: { type: String, required: true }, 
+    image: { type: String, required: true } 
 });
 
-// Создаем модель продукта
 const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
